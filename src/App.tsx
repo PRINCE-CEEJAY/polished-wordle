859fd39e3fe894e7db23fac617495009b198bcc4
PRINCE CEEJAY NG <92728956+PRINCE-CEEJAY@users.noreply.github.com>
2025-11-25 13:21:54 +0000

    added shadcn and loading bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-export default function App(){
-  return <div>
-    <h1>Home</h1>
-  </div>
+import Loader from './components/Loader';
+
+export default function App() {
+  return (
+    <div className='flex flex-col justify-center items-center min-h-screen'>
+      <h1>Home</h1>
+      <Loader />
+    </div>
+  );
 }
---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+export default function Loader() {
+  return (
+    <div className='flex justify-center items-center gap-2 loading'>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
